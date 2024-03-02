72fbbc6fb3325e6675fc45adc65a8f244bd9b150
Shuxin Wang <81904920+A-BigTree@users.noreply.github.com>
2024-03-02 10:51:26 +0800

    Update config.py

--- common/config.py
+++ common/config.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 import yaml
 
-with open("../config.yaml", "r", encoding="utf-8") as f:
+with open("../config.yml", "r", encoding="utf-8") as f:
     CONFIG = yaml.load(f.read(), Loader=yaml.FullLoader)
 
 # datasets config
@@ -16,7 +16,7 @@ SOLR_UPDATE_DATA_DIR: str = CONFIG["solr"]["update"]["data-dir"]
 SOLR_UPDATE_URL: str = CONFIG["solr"]["update"]["update-url"]
 SOLR_UPDATE_QUEUE_SIZE: int = CONFIG["solr"]["update"]["queue-size"]
 SOLR_UPDATE_PRODUCE_THREAD: int = CONFIG["solr"]["update"]["produce-thread"]
-SOLR_UPDATE_CONSUMER_THREAD: int = CONFIG["solr"]["update"]["consumer-thread"]
+SOLR_UPDATE_CONSUMER_THREAD: int = CONFIG["solr"]["update"]["consume-thread"]
 # Solr query
 SOLR_QUERY_URL: str = CONFIG["solr"]["query"]["query-url"]
 SOLR_QUERY_PARAMS: dict = CONFIG["solr"]["query"]["query-params"]
