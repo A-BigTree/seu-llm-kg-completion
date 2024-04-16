69fb2e9c9e348344faa614e9c6b1e3b743296f0c
Shuxin Wang <shuxinwang662@gmail.com>
2024-04-16 17:36:31 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,8 +4,8 @@ import sys
 object_path = os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
 sys.path.append(object_path)
 
-from tasks.train import *
-from tasks.task import *
+# from tasks.train import *
+# from tasks.task import *
 
 if __name__ == '__main__':
     # task = SolrInitTask()
@@ -13,5 +13,6 @@ if __name__ == '__main__':
     # task = PreProcessTask()
     # task = GPTRequestTask()
     # task = TextEmbeddingTask()
-    task = MFTrainTask()
-    task.run_task()
+    # task = MFTrainTask()
+    # task.run_task()
+    print('Hello World')
